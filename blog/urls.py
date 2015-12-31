584c005c927deeaac43070c0d7c0d4be321bea32
Ruto <winstonwut@gmail.com>
2015-12-30 21:23:16 -0500

    Base view of blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /blog/
     url(r'^$', views.index, name='index'),
+    # /blog/<id>
+    url(r'^(?P<id>[0-9]+)/$', views.id, name='id'),
+    # /blog/<word>
+    url(r'^(?P<word>.*)/$', views.word, name='word'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import BlogPost
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ASDF")
+    posts = get_list_or_404(BlogPost)
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+
+def id(request, id):
+    post = get_object_or_404(BlogPost, pk=id)
+    context = {'post': post}
+    return render(request, 'blog/index.html', context)
+
+
+def word(request, word):
+    post = get_object_or_404(BlogPost, title=word)
+    context = {'post': post}
+    return render(request, 'blog/index.html', context)
